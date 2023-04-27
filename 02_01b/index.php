e7c893b50953f0cf2fbf4611fbc81cc9e722b0cb
Pedro Sousa <63027972+pedro-s-20@users.noreply.github.com>
2023-04-27 16:19:27 +0000

    questoes 04 e 05 mod 1 e questao 01 mod 2

--- 02_01b/index.php
+++ 02_01b/index.php
@@ -1,2 +1,11 @@
 <?php
 echo 'Create your list of choices: ' . "\n\n";
+
+$choices = array();
+
+do {
+  $choices[] = readline('Fazer uma escolha? (digite 0 para parar)');
+} while ('0' != end($choices));
+
+array_pop($choices);
+echo 'E a decisão é...' . $choices[array_rand($choices)];
