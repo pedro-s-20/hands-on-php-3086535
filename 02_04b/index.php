90b0343c9a78e91cebfa574d4c4d0c9c58ddfdea
Pedro Sousa <63027972+pedro-s-20@users.noreply.github.com>
2023-05-01 22:29:46 +0000

    modulo 02 finalizado

--- 02_04b/index.php
+++ 02_04b/index.php
@@ -1,16 +1,32 @@
-<?php 
-	function get_visitors() {
-		return file_get_contents('visits.txt');
-	}
+<?php
+function get_visitors()
+{
+    return file_get_contents('visits.txt');
+}
+
+function log_visit()
+{
+    $visits = (int) file_get_contents('visits.txt');
+    $visits++;
+    file_put_contents('visits.txt', $visits);
+}
+
+if (!isset($_COOKIE['visited'])) {
+    setcookie('visited', true, time() + (86400 * 30));
+    log_visit();
+}
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Welcome to My Website</title>
-        <meta name="author" value="Joe Casabona" />
-    </head>
-    <body>
-        <h1>Welcome to My Website</h1>
-				<p>There have been <b><?php echo get_visitors(); ?></b> visitors so far.</p>
-    </body>
+
+<head>
+    <title>Welcome to My Website</title>
+    <meta name="author" value="Joe Casabona" />
+</head>
+
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>There have been <b><?php echo get_visitors(); ?></b> visitors so far.</p>
+</body>
+
 </html>
